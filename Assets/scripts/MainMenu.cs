caf5b566000408c7225197820b1b5033e75f4ff1
Jamoliddinkhuja <bloodjey98@gmail.com>
2019-04-28 19:06:56 +0500

    Main Menu

---
+++ Assets/scripts/MainMenu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    public string Playgamelevel;
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(Playgamelevel);
+    }
+    public void QuitGame()
+    {
+        Debug.Log("QUIT");
+        Application.Quit();
+    }
+}
