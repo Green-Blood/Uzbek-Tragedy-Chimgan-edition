08c41dfea9740742200e2059236d8b57d3f55d77
Jamoliddinkhuja <bloodjey98@gmail.com>
2019-05-03 02:11:27 +0500

    Score

---
+++ Assets/scripts/HighScore.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighScore {
+
+	public int Score { get; set;}
+	public string Name {get; set;}
+	public int ID { get; set;}
+	public HighScore(int id, string name, int score)
+	{
+		this.Score = score;
+		this.Name = name;
+		this.ID = id;
+
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/scripts/HighScoreManager.cs
@@ -0,0 +1,91 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.Text;
+using System.IO;
+using System.Collections.Generic;
+using Yarn.Unity;
+using Mono.Data.Sqlite;
+using System;
+using System.Data;
+
+public class HighScoreManager : MonoBehaviour {
+
+	private string conn, sqlQuery;
+    IDbConnection dbconn;
+    IDbCommand dbcmd;
+	public GameObject scorePrefab;
+	public Transform scoreParent;
+	// Use this for initialization
+	//Connect
+	private List<HighScore> highscores = new List<HighScore>();
+	void Start () {
+		conn = "URI=file:" + Application.dataPath + "/gamedb.db"; 
+		//GetScore();
+		ShowScores();  
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	private void GetScore()
+	{
+		highscores.Clear();
+		using(IDbConnection dbConnection = new SqliteConnection(conn))
+		{
+			dbConnection.Open();
+			using(IDbCommand dbCmd = dbConnection.CreateCommand())
+			{
+				string sqlQuery = "SELECT * FROM flags";
+				dbCmd.CommandText = sqlQuery;
+				using(IDataReader reader = dbCmd.ExecuteReader())
+				{
+					while(reader.Read())
+					{
+						highscores.Add(new HighScore(reader.GetInt32(0), reader.GetString(1), reader.GetInt32(2)));	
+					}
+					dbConnection.Close();
+					reader.Close();
+				}
+
+			}
+		}
+	}
+	private void insertvalue(int id, string name)
+		{
+			using (dbconn = new SqliteConnection(conn))
+			{
+				dbconn.Open(); //Open connection to the database.
+				dbcmd = dbconn.CreateCommand();
+				sqlQuery = string.Format("INSERT INTO Characters (ID, Name) values (\"{0}\",\"{1}\")",id,name);// table name
+				dbcmd.CommandText = sqlQuery;
+				dbcmd.ExecuteScalar();
+				dbconn.Close();
+			}
+		}
+		private void deletevalue(int id)
+		{
+			using (dbconn = new SqliteConnection(conn))
+			{
+				dbconn.Open(); //Open connection to the database.
+				dbcmd = dbconn.CreateCommand();
+				sqlQuery = string.Format("DELETE FROM Characters WHERE ID=\"{0}\"", id);// table name
+				dbcmd.CommandText = sqlQuery;
+				dbcmd.ExecuteScalar();
+				dbconn.Close();
+			}
+		}
+	private void ShowScores()
+	{
+		GetScore();
+		for(int i = 0; i < highscores.Count; i++)
+		{
+			GameObject tmpObject = Instantiate(scorePrefab);
+			HighScore tmpScore = highscores[i];
+			tmpObject.GetComponent<HighScoreScript>().SetScore("#" + (i+1).ToString(), tmpScore.Name, tmpScore.Score.ToString());
+			tmpObject.transform.SetParent(scoreParent);
+			//tmpObject.GetComponent<RectTransform().localScale = new Vector3(1,1,1);
+		}
+	}
+}
